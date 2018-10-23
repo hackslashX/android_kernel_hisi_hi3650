ccb96351386b67e77e597a7ec215e0bfb61a8abf
NeonDragon1909 <nahil123@live.com>
2018-10-23 20:59:42 +0500

    drivers: hisi: pm: hide references to dubai if it is disabled

--- drivers/hisi/pm/pm.c
+++ drivers/hisi/pm/pm.c
@@ -144,8 +144,10 @@ void pm_gic_pending_dump(void)
 				if (gpio >= 0) {
 					printk("(gpio-%d)", gpio);
                 }
+#ifdef CONFIG_HUAWEI_DUBAI
 				/* notify dubai module to update wakeup information */
 				// dubai_update_wakeup_info(g_ap_irq_name[irq], gpio); -> disable dubai
+#endif
 				printk("\n");
 			}
 		}
