5404d2e7fe08ee6112dd12683e8e647996f9a127
Clement Leger <cleger@kalray.eu>
2019-10-04 09:37:36 +0200

    remoteproc: Fix wrong rvring index computation
    commit 00a0eec59ddbb1ce966b19097d8a8d2f777e726a upstream.
    
    Index of rvring is computed using pointer arithmetic. However, since
    rvring->rvdev->vring is the base of the vring array, computation
    of rvring idx should be reversed. It previously lead to writing at negative
    indices in the resource table.
    
    Signed-off-by: Clement Leger <cleger@kalray.eu>
    Link: https://lore.kernel.org/r/20191004073736.8327-1-cleger@kalray.eu
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Cc: Doug Anderson <dianders@chromium.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/remoteproc/remoteproc_core.c
+++ drivers/remoteproc/remoteproc_core.c
@@ -316,7 +316,7 @@ void rproc_free_vring(struct rproc_vring *rvring)
     int size = PAGE_ALIGN(vring_size(rvring->len, rvring->align));
 #endif
 	struct rproc *rproc = rvring->rvdev->rproc;
-	int idx = rvring->rvdev->vring - rvring;
+	int idx = rvring - rvring->rvdev->vring;
 	struct fw_rsc_vdev *rsc;
 #ifndef CONFIG_HISI_REMOTEPROC_DMAALLOC_DEBUG
 	size = PAGE_ALIGN(vring_size(rvring->len, rvring->align));
