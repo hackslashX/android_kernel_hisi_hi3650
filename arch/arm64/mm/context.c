b28837088c6e8ed712020d6d991b65c9dd4641f5
Muhammad Fahad Baig <fadi@fahadbaig.com>
2019-11-04 21:58:21 +0000

    Revert "arm64: Rename cpuid_feature field extract routines"
    This reverts commit 7406c983427b3a86bcac86a4d7c7eae0a0815ac2.

--- arch/arm64/mm/context.c
+++ arch/arm64/mm/context.c
@@ -201,8 +201,7 @@ switch_mm_fastpath:
 
 static int asids_init(void)
 {
-	int fld = cpuid_feature_extract_unsigned_field(read_cpuid(ID_AA64MMFR0_EL1),
-						       ID_AA64MMFR0_ASID_SHIFT);
+	int fld = cpuid_feature_extract_field(read_cpuid(SYS_ID_AA64MMFR0_EL1), 4);
 
 	switch (fld) {
 	default:
