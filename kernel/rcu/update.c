64589fd2a9c9ab4aea1e5843aab9e8ed04919932
aled99 <bumbi@live.it>
2020-07-09 23:08:04 +0200

    rcu: Speed up calling of RCU tasks callbacks
    RCU tasks callbacks can take at least 1 second before the callbacks are
    executed. This happens even if the hold-out tasks enter their quiescent states
    quickly. I noticed this when I was testing trampoline callback execution.
    
    To test the trampoline freeing, I wrote a simple script:
    cd /sys/kernel/debug/tracing/
    echo '__schedule_bug:traceon' > set_ftrace_filter;
    echo '!__schedule_bug:traceon' > set_ftrace_filter;
    
    In the background I had simple bash while loop:
    while [ 1 ]; do x=1; done &
    
    Total time of completion of above commands in seconds:
    
    With this patch:
    real    0m0.179s
    user    0m0.000s
    sys     0m0.054s
    
    Without this patch:
    real    0m1.098s
    user    0m0.000s
    sys     0m0.053s
    
    That's a greater than 6X speed up in performance. In order to accomplish
    this, I am waiting for HZ/10 time before entering the hold-out checking
    loop. The loop still preserves its checking of held tasks every 1 second
    as before, in case this first test doesn't succeed.
    
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Peter Zilstra <peterz@infradead.org>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Boqun Feng <boqun.feng@gmail.com>
    Cc: Paul McKenney <paulmck@linux.vnet.ibm.com>
    Cc: byungchul.park@lge.com
    Cc: kernel-team@android.com
    Signed-off-by: Joel Fernandes (Google) <joel@joelfernandes.org>
    Signed-off-by: danny aeng <noreference@web.de>

--- kernel/rcu/update.c
+++ kernel/rcu/update.c
@@ -725,6 +725,12 @@ static int __noreturn rcu_tasks_kthread(void *arg)
 		synchronize_srcu(&tasks_rcu_exit_srcu);
 
 		/*
+		 * Wait a little bit incase held tasks are released
+		 * during their next timer ticks.
+		 */
+		schedule_timeout_interruptible(HZ/10);
+
+		/*
 		 * Each pass through the following loop scans the list
 		 * of holdout tasks, removing any that are no longer
 		 * holdouts.  When the list is empty, we are done.
@@ -749,7 +755,6 @@ static int __noreturn rcu_tasks_kthread(void *arg)
 			if (fract > 1)
 				fract--;
 
-
 			rtst = READ_ONCE(rcu_task_stall_timeout);
 			needreport = rtst > 0 &&
 				     time_after(jiffies, lastreport + rtst);
@@ -762,6 +767,11 @@ static int __noreturn rcu_tasks_kthread(void *arg)
 				check_holdout_task(t, needreport, &firstreport);
 				cond_resched();
 			}
+
+			if (list_empty(&rcu_tasks_holdouts))
+				break;
+
+			schedule_timeout_interruptible(HZ);
 		}
 
 		/*
