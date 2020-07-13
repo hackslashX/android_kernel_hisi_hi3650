3a64bc3c46bd03bb58fd844872971f12c522a8db
aled99 <bumbi@live.it>
2020-07-13 22:13:48 +0200

    disable log

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -9192,6 +9192,6 @@ struct cgroup_subsys cpu_cgrp_subsys = {
 
 void dump_cpu_task(int cpu)
 {
-	pr_info("Task dump for CPU %d:\n", cpu);
+	pr_debug("Task dump for CPU %d:\n", cpu);
 	sched_show_task(cpu_curr(cpu));
 }
