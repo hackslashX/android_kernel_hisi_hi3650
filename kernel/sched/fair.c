ac05c8abc2455627056ab2aa0552cad05a6a5fad
aled99 <bumbi@live.it>
2020-08-04 17:57:35 +0200

    fixes

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -56,8 +56,8 @@
  * (to see the precise effective timeslice length of your workload,
  *  run vmstat and monitor the context-switches (cs) field)
  */
-unsigned int sysctl_sched_latency = 5000000ULL;
-unsigned int normalized_sysctl_sched_latency = 5000000ULL;
+unsigned int sysctl_sched_latency = 6000000ULL;
+unsigned int normalized_sysctl_sched_latency = 6000000ULL;
 
 unsigned int sysctl_sched_is_big_little = 1;
 unsigned int sysctl_sched_sync_hint_enable = 1;
@@ -5834,7 +5834,7 @@ static int wake_affine(struct sched_domain *sd, struct task_struct *p, int sync)
 		prev_eff_load *= load + effective_load(tg, prev_cpu, 0, weight);
 	}
 
-	balanced = this_eff_load <= prev_eff_load ? this_cpu : nr_cpumask_bits;
+	balanced = this_eff_load <= prev_eff_load;
 
 	schedstat_inc(p, se.statistics.nr_wakeups_affine_attempts);
 
@@ -6372,7 +6372,7 @@ static int select_idle_sibling(struct task_struct *p, int target)
 {
 	struct sched_domain *sd;
 	struct sched_group *sg;
-	int i = task_cpu(p), recent_used_cpu;
+	int i = task_cpu(p);
 	int best_idle = -1;
 	int best_idle_cstate = -1;
 	int best_idle_capacity = INT_MAX;
@@ -6385,23 +6385,7 @@ static int select_idle_sibling(struct task_struct *p, int target)
 		 * If the prevous cpu is cache affine and idle, don't be stupid.
 		 */
 		if (i != target && cpus_share_cache(i, target) && idle_cpu(i))
-			return idle_cpu(prev) ? prev : i;
-		}
-
-		/* Check a recently used CPU as a potential idle candidate */
-		recent_used_cpu = p->recent_used_cpu;
-		if (recent_used_cpu != prev &&
-	    		recent_used_cpu != target &&
-	    		cpus_share_cache(recent_used_cpu, target) &&
-	    		idle_cpu(recent_used_cpu) &&
-	    		cpumask_test_cpu(p->recent_used_cpu, &p->cpus_allowed)) {
-
-			 /*
-		 	  * Replace recent_used_cpu with prev as it is a potential
-		 	  * candidate for the next wake.
-		  	  */
-			 p->recent_used_cpu = prev;
-			return recent_used_cpu;
+			return i;
 	}
 
 	/*
@@ -6926,7 +6910,8 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
 
 	if (affine_sd) {
 		sd = NULL; /* Prefer wake_affine over balance flags */
-		new_cpu = wake_affine(affine_sd, p, prev_cpu, sync);
+		if (cpu != prev_cpu && wake_affine(affine_sd, p, sync))
+			new_cpu = cpu;
 	}
 
 #ifdef CONFIG_HISI_EAS_SCHED
@@ -6947,11 +6932,8 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
 	}
 
 	if (!sd) {
-		if (sd_flag & SD_BALANCE_WAKE) { /* XXX always ? */
+		if (sd_flag & SD_BALANCE_WAKE) /* XXX always ? */
 			new_cpu = select_idle_sibling(p, new_cpu);
-			if (want_affine)
-				current->recent_used_cpu = cpu;
-		}
 
 	} else while (sd) {
 		struct sched_group *group;
